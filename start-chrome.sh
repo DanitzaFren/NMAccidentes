07649f16e52cb5e2e04ec95e303c14a980c5d038
daniifreen <d.fren@alumnos.duoc.cl>
2020-12-09 09:58:26 -0300

    prueba

--- start-chrome.sh
+++ start-chrome.sh
@@ -2,16 +2,16 @@
 
 # Run Chrome via Selenium Server
 start-chrome() {
-    xvfb-run java -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -jar /usr/local/bin/selenium-server-standalone.jar
+    xvfb-run java -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -jar /usr/local/bin/selenium-server-standalone.jar;
 }
 
 start-chrome-debug() {
-    xvfb-run java -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -jar /usr/local/bin/selenium-server-standalone.jar -debug
+    xvfb-run java -Dwebdriver.chrome.driver=/usr/local/bin/chromedriver -jar /usr/local/bin/selenium-server-standalone.jar -debug;
 }
 
 # Run Chrome Headless
 start-chrome-headless() {
-    chromedriver --url-base=/wd/hub
+    chromedriver --url-base=/wd/hub;
 }
 
 # Start
