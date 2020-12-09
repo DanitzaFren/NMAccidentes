d57edf9c99e1cd8cfc8b40cf76d53fc8c4d55f76
daniifreen <d.fren@alumnos.duoc.cl>
2020-12-09 11:13:18 -0300

    prueba

--- Pruebas/prueba1.py
+++ Pruebas/prueba1.py
@@ -12,7 +12,7 @@ class usando_unitest(unittest.TestCase):
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        self.driver = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=chrome_options)
+        self.driver = webdriver.Chrome('/usr/local/bin/chromedriver')
 # Inicio de sesion del Administrador
     def test_iniciar_sesion_admin(self):
         driver = self.driver
