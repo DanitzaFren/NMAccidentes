268e5c28dc9f4e08e0f9acc11d28f453c3105dd4
daniifreen <d.fren@alumnos.duoc.cl>
2020-12-10 19:16:37 -0300

    id

--- Pruebas/prueba4.py
+++ Pruebas/prueba4.py
@@ -5,7 +5,6 @@ from selenium.webdriver.support.select import Select
 from selenium.webdriver.common.alert import Alert
 import time
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 #### pruebas de ingresar 
 class usando_unitest(unittest.TestCase):
 
@@ -19,43 +18,38 @@ class usando_unitest(unittest.TestCase):
     def test_Crear_profesional(self):
         driver = self.driver
         driver.maximize_window()
-        driver.implicitly_wait(20)
         driver.get("http://127.0.0.1:8000/accounts/login/")
         self.assertIn("Login", driver.title)
         usuario = driver.find_element_by_id("id_username")
-        usuario.send_keys("Benja")
+        usuario.send_keys("admin")
         clave = driver.find_element_by_id("id_password")
-        clave.send_keys("duoc123456")
+        clave.send_keys("admin")
         usuario.send_keys(Keys.ENTER)
-        time.sleep(5)
-        #servicio
-        servicio = driver.find_element_by_xpath("/html/body/header/div/nav/ul/li[4]/a").click()
-        time.sleep(5)
-        #Crear profesiional
-        driver.find_element_by_xpath("/html/body/main/section/div/div/div[2]/div/div[2]/input").click()
+        driver.get("http://127.0.0.1:8000/servicios")
         time.sleep(2)
+        driver.get("http://127.0.0.1:8000/crearProfesionales_N.html")
         self.assertIn("Ingresar Profesional", driver.title)
         rut = driver.find_element_by_id("txtRut")
-        rut.send_keys("197927035")
+        rut.send_keys("197927038")
         nombre = driver.find_element_by_id("id_nombre")
-        nombre.send_keys("Benjami")
+        nombre.send_keys("Benjamin")
         paterno = driver.find_element_by_id("id_paterno")
-        paterno.send_keys("Brav")
+        paterno.send_keys("Bravo")
         materno = driver.find_element_by_id("id_materno")
-        materno.send_keys("Dia")
-        driver.find_element_by_css_selector("#main > section.contact.aos-init.aos-animate > div > div > div.col-lg-12 > form > center > button").click()
+        materno.send_keys("Diaz")
+        paterno.send_keys(Keys.ENTER)
         time.sleep(2)
         username = driver.find_element_by_id("id_username")
-        username.send_keys("Profesional6")
+        username.send_keys("Profesional")
         contraa = driver.find_element_by_id("id_password1")
         contraa.send_keys("duoc123456")
         contras = driver.find_element_by_id("id_password2")
         contras.send_keys("duoc123456")
         email = driver.find_element_by_id("id_email")
-        email.send_keys("prueba@gmail.com6")
-        driver.find_element_by_css_selector("#main > section.contact.aos-init.aos-animate > div > div > div.col-lg-12 > form > center > button").click()
+        email.send_keys("prueba@gmail.com")
+        username.send_keys(Keys.ENTER)
         time.sleep(2)
-        if driver.find_element_by_css_selector("#main > section.contact.aos-init.aos-animate > div > div > div.alert.alert-info").text=='Usuario: Profesional registrado correctamente.':
+        if driver.current_url=='http://127.0.0.1:8000/register_Profesional':
             print("Se a creado el profesional con exito ")
         else:
             print("no se pudo ingresar el profesional")
@@ -65,5 +59,6 @@ class usando_unitest(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
 #login_url = 'https://account.domaintools.com/log-in/?r=https%3A%2F%2Freversewhois.domaintools.com%2F%3Frefine'
 #login = driver.find_element_by_xpath('//a[@href="'+login_url+'"]').click()
